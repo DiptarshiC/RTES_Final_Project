975cb2dd8865e1c4873cb7d0ca45f7d4a3e6d9ad
Diptarshi Chakraborty <31807400+DiptarshiC@users.noreply.github.com>
2018-08-03 16:45:34 -0600

    sequencer working

--- capture.cpp
+++ capture.cpp
@@ -19,7 +19,9 @@
 
 
 #include <X11/Xlib.h>
+#include <sys/sysinfo.h>
 #include <semaphore.h>
+#include <sched.h>
 #include <pthread.h>
 #include <syslog.h>
 #include <time.h>
@@ -41,7 +43,7 @@ using namespace std;
 
 #define HRES 		640
 #define VRES 		480
-#define NUM_OF_THREADS	3
+#define NUM_OF_THREADS	4
 
 
 /****************************VARIABLES*********************************************/
@@ -59,9 +61,9 @@ pthread_t save_t;				//thread for saving
 Mat FRAME;					//matrix for storing the image
 
 pthread_mutex_t mylock;				//lock to synchronize the frame variable
-sem_t A;
-sem_t B;
-sem_t C;
+sem_t A;					//semaphore for capture thread.
+sem_t B;					//semaphore for the scan thread
+sem_t C;					//semaphore for the save thread
 
 // Transform display window
 char timg_window_name[] = "Edge Detector Transform";
@@ -80,6 +82,33 @@ char file_name[100];
 
 char tag_number[50];
 
+void *sequencer(void *arg)//sequence_t will point to this
+{
+
+	struct timespec tv1;
+	struct timespec tv2;
+	tv1.tv_sec=0;
+	tv1.tv_nsec=50000000;
+
+
+	int count=0;
+	while(1)
+	{	sem_post(&A);
+		
+		if(count%2==0)
+		{
+		sem_post(&B);
+		}
+		
+		if(count%5==0)
+		{
+		sem_post(&C);
+		}
+		count++;
+		nanosleep(&tv1,&tv2);
+	}
+
+}
 
 
 void *capture1(void *arg)//capture_t will point to this
@@ -87,8 +116,9 @@ void *capture1(void *arg)//capture_t will point to this
 	char key;
 
 	printf("\nThread1");
-	if(!capture.isOpened()){
-	printf("\nerror in opening");	
+	if(!capture.isOpened())
+	{
+		printf("\nerror in opening");	
 	}
     	while(1)
     	{
@@ -106,11 +136,12 @@ void *capture1(void *arg)//capture_t will point to this
 	pthread_mutex_unlock(&mylock);
 	
 	key=waitKey(1);
-	if(key==27){
+	if(key==27)
+	{
 	printf("\nExiting the application");
 	break;
 	}
-	sem_post(&B);
+	
     	}
 
     
@@ -118,8 +149,6 @@ void *capture1(void *arg)//capture_t will point to this
 
     
 }
-
-
 
 
 void *scan(void *arg)//scan_t will point to this
@@ -136,16 +165,16 @@ void *scan(void *arg)//scan_t will point to this
  	
 	
 	pthread_mutex_lock(&mylock);
- 	//imshow( "Scan Thread", FRAME );
+ 	imshow( "Scan Thread", FRAME );
 	pthread_mutex_unlock(&mylock);
 
  	key=waitKey(1);
-	if(key==27){
+	if(key==27)
+	{
 	printf("\nExiting the application");
 	break;
 	}
-	//sleep(0.09);
-	sem_post(&C);
+	
 	}
 }
 
@@ -153,7 +182,7 @@ void *scan(void *arg)//scan_t will point to this
 void *save(void *arg)//save_t will point to this
 {	
 	
-	int count=1;
+	//int count=1;
 	printf("\nThread3");
 	char key;
 
@@ -165,22 +194,22 @@ void *save(void *arg)//save_t will point to this
  	
 	
 	pthread_mutex_lock(&mylock);
- 	//imshow( "Save Thread", FRAME );
-	strcpy(file_name,"");
-	sprintf(tag_number,"%04d",count);
-	strcat(file_name,tag_number);
-	strcat(file_name,".jpg");
-	count++;
-	imwrite(file_name,FRAME);
+ 	imshow( "Save Thread", FRAME );
+	//strcpy(file_name,"");
+	//sprintf(tag_number,"%04d",count);
+	//strcat(file_name,tag_number);
+	//strcat(file_name,".jpg");
+	//count++;
+	//imwrite(file_name,FRAME);
 	pthread_mutex_unlock(&mylock);
 
  	key=waitKey(1);
-	if(key==27){
+	if(key==27)
+	{
 	printf("\nExiting the application");
 	break;
 	}
-	sleep(1);
-	sem_post(&A);
+	
 	}
 }
 
@@ -194,7 +223,8 @@ int main( int argc, char** argv )
 {
 
 
-
+	int coreid;
+	cpu_set_t threadcpu;
 
 	
 	status=XInitThreads();
@@ -203,7 +233,14 @@ int main( int argc, char** argv )
 	else
 	printf("\n The X Window system initialized successfully\n");
 
+	printf("This system has %d processors configured and %d processors available.\n", get_nprocs_conf(), get_nprocs());
 
+	int numberOfProcessors = get_nprocs_conf();
+ 
+	printf("number of CPU cores=%d\n", numberOfProcessors);
+
+	
+	
 
 	struct sched_param rt_param[NUM_OF_THREADS];
     	pthread_attr_t rt_sched_attr[NUM_OF_THREADS];
@@ -232,15 +269,20 @@ int main( int argc, char** argv )
 	pthread_attr_init(&rt_sched_attr[i]);
         pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
         pthread_attr_setschedpolicy(&rt_sched_attr[i],  SCHED_FIFO);
+	//pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
 	    
-	    //Assigning highest priority to the 0th index which is used for Sum1 and onwards
-	    rt_param[i].sched_priority = rt_max_prio - (i + 1);
+	    
+	    //rt_param[i].sched_priority = rt_max_prio - (i + 1);
 	    pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
 	}
+	
+	
+	rt_param[0].sched_priority = rt_max_prio - (1);
+	rt_param[1].sched_priority = rt_max_prio - (2);
+	rt_param[2].sched_priority = rt_max_prio - (3);
+	rt_param[0].sched_priority = rt_max_prio - (3);
 
-
-
-	sem_init(&A, 0, 1);
+	sem_init(&A, 0, 0);
         sem_init(&B, 0, 0);
 	sem_init(&C, 0, 0);
 
@@ -249,7 +291,19 @@ int main( int argc, char** argv )
 
 	printf("\n let us begin ");
 
-	status=pthread_create(&capture_t,&rt_sched_attr[0],capture1,NULL);//creation of thread
+	
+
+	status=pthread_create(&sequence_t,&rt_sched_attr[0],sequencer,NULL);//creation of thread
+        if(status==0)
+	{
+	printf("\n Sequencer thread  creation successfull");
+	}
+	else
+	printf("\n Sequencer thread  creation unsuccessfull");
+	
+	
+
+	status=pthread_create(&capture_t,&rt_sched_attr[1],capture1,NULL);//creation of thread
         if(status==0)
 	{
 	printf("\n Capture thread  creation successfull");
@@ -259,7 +313,7 @@ int main( int argc, char** argv )
 	
 
 	printf("\n let us create the second thread ");
-	status=pthread_create(&scan_t,&rt_sched_attr[1],scan,NULL);//creation of thread
+	status=pthread_create(&scan_t,&rt_sched_attr[2],scan,NULL);//creation of thread
         if(status==0)
 	{
 	printf("\n Scan thread creation successfull");
@@ -269,7 +323,7 @@ int main( int argc, char** argv )
 
 	
 	printf("\n let us create the third thread ");
-	status=pthread_create(&save_t,&rt_sched_attr[2],save,NULL);//creation of thread
+	status=pthread_create(&save_t,&rt_sched_attr[3],save,NULL);//creation of thread
         if(status==0)
 	{
 	printf("\n Save thread creation successfull");
@@ -278,6 +332,7 @@ int main( int argc, char** argv )
 	printf("\n Save thread creation unsuccessfull");
 
 
+	pthread_join(sequence_t,NULL);
 	pthread_join(capture_t,NULL);
 	pthread_join(scan_t,NULL);
 	pthread_join(save_t,NULL);	
